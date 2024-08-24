2992fe6b11bc28bb2ceecaf0ebcfed72524a7b1f
ceo-s <alex_s_2002@mail.ru>
2024-08-24 14:00:11 +0300

    Test workflows 8

--- build_linux.sh
+++ build_linux.sh
@@ -1,2 +1,2 @@
-pyinstaller --distpath repeat_shit -F --contents-directory . --hidden-import PIL._tkinter_finder main.py
+pyinstaller --distpath repeat_shit --add-data="assets:assets" -F --contents-directory . --hidden-import PIL._tkinter_finder main.py
 tar -zcvf linux-build.tar.gz repeat_shit
